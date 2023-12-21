79abde6d2572e956cb1e3adc8d6ddcc7a5a87984
Rafagio <giovanetti610@gmail.com>
2023-12-21 16:29:37 -0300

    alteracao de difuculdade limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+//alteracao deixar mais dificil
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
